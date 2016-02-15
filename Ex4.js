15adb852deee5ed28b6d159a705a119959a54f3f
Ethan Ward <ethanandrewward@gmail.com>
2016-02-15 17:39:03 -0600

    Initial commit

---
+++ Ex4.js
@@ -0,0 +1,22 @@
+function changeColor() {
+
+  var paragraph = document.getElementById("text");
+
+  var borderR = document.getElementById("borderRvalue");
+  var borderG = document.getElementById("borderGvalue");
+  var borderB = document.getElementById("borderBvalue");
+  var borderWidth = document.getElementById("width");
+
+  var backR = document.getElementById("backRvalue");
+  var backG = document.getElementById("backGvalue");
+  var backB = document.getElementById("backBvalue");
+
+  var borderRGB = "#" + borderR.value + borderG.value + borderB.value;
+  var backRGB = "#" + backR.value + backG.value + backB.value;
+  var width = borderWidth.value;
+
+  paragraph.style.borderColor = "red";//borderRGB;
+  paragraph.style.borderWidth = "thick";
+  paragraph.style.backgroundColor = backRGB;
+
+}
